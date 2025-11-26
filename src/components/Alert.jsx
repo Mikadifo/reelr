37ecb7bbd4879583f91f9ce85f3040058cb9cf7b
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-11-26 17:47:59 -0500

    mod: self closing alert after 3s

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,6 +1,13 @@
 import CloseIcon from "@assets/icons/cross.svg?react";
+import { useEffect } from "react";
 
 function Alert({ alert, setAlert }) {
+  useEffect(() => {
+    if (alert.open) {
+      setTimeout(() => setAlert({ ...alert, open: false }), 3000);
+    }
+  }, [alert, setAlert]);
+
   return (
     <div
       className={`${alert.severity === "error" ? "bg-red text-white" : "bg-green text-dark"} font-body px-6 py-3 rounded-2xl whitespace-nowrap absolute bottom-6 left-1/2 -translate-x-1/2 flex gap-4 z-40`}
